66ccdfa00bbf7e2c5abce1ccb257c7150af92b43
llobato <louplelpro@gmail.com>
2022-09-23 15:40:16 +0200

    bouton reset

--- src/App.vue
+++ src/App.vue
@@ -4,48 +4,76 @@ import { createClient } from '@supabase/supabase-js'
 import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/SupabaseAuthClient'
 </script>
 
-<template>    
+<template>
   <header>
     <router-link to="/">Go to Home</router-link>
     <img alt="Logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
     <div class="wrapper" id="signOut">
-      <div><SignIn msg="User, please sign in !" /></div>
+      <div>
+        <SignIn msg="User, please sign in !" />
+      </div>
       <label>email: </label><br>
-	    <input type="email" required v-model="email" placeholder="username@domain.tld"><br>
-	    <label>password: </label><br>
-	    <input type="password" required v-model="passwd" ><br>
+      <input type="email" required v-model="email" placeholder="username@domain.tld"><br>
+      <label>password: </label><br>
+      <input type="password" required v-model="passwd"><br>
       <button v-on:click="register()">Sign Up</button>
       <button v-on:click="login()">Sign In</button>
+      <button v-on:click="reset()">Reset</button>
       <p>
-      <label id="status"> You are not yet connected </label><br>  
+        <label id="status"> You are not yet connected </label><br>
       </p>
     </div>
   </header>
-  
+
   <main>
-    
+
   </main>
 </template>
 
 <script>
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL'
-const SUPABASE_KEY = 'YOUR_ANON_KEY'
+const SUPABASE_URL = 'https://fxyszmnjhspnkamjyfmw.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4eXN6bW5qaHNwbmthbWp5Zm13Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMzMzk4MjQsImV4cCI6MTk3ODkxNTgyNH0.gpqLNi3qKVqT7CruqwHiAh1ftDc3Ohqg8WVFWYFHSQ8'
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
 
 export default {
-  methods: {  
+  methods: {
     //this method allows a new user to sign up the system. Once done, the user receives an email
     //asking for account validation. Once the validation made the user is added to the system
-    async register(){
-      
+    async register() {
+      try {
+        const { user, session, error } = await supabase.auth.signUp({
+          email: this.email,
+          password: this.passwd,
+        });
+        if (error) throw error;
+        document.getElementById('status').innerHTML = 'Please validate the received email!'
+      } catch (error) {
+        alert(error.error_description || error.message);
+      }
     },
     //this method allows the already registred user to log in the system.
-    async login(){
-       
-    }
-  }  
+    async login() {
+      alert("je suis la");
+      try {
+        const { user, session, error } = await supabase.auth.signIn({
+          email: this.email,
+          password: this.passwd,
+        });
+        if (error) throw error;
+        document.getElementById('status').innerHTML = 'You are now logged !'
+      } catch (error) {
+        alert(error.error_description || error.message);
+      }
+    },
+
+    async reset() {
+      const { data, error } = await supabase.auth.api.resetPasswordForEmail(
+        this.email
+      )
+    },
+  }
 }
 </script>
 
@@ -53,12 +81,12 @@ export default {
 @import './assets/base.css';
 
 header .hidden {
-    visibility: hidden;
-    overflow: hidden;
-    display: flex;
-    display:inline-block;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  visibility: hidden;
+  overflow: hidden;
+  display: flex;
+  display: inline-block;
+  place-items: flex-start;
+  flex-wrap: wrap;
 }
 
 #app {
@@ -111,7 +139,7 @@ a,
 
   header .wrapper {
     display: flex;
-    display:inline-block;
+    display: inline-block;
     place-items: flex-start;
     flex-wrap: wrap;
   }
